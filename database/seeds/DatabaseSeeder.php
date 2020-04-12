6ab43e86dcd632c32a8ce0444f81a135546ee608
nhatuk2710 <49880246+nhatuk2710@users.noreply.github.com>
2020-04-12 22:18:44 +0700

    Update DatabaseSeeder.php

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,6 +14,6 @@ class DatabaseSeeder extends Seeder
 //        $this->call(CategorySeeder::class);
 //        $this->call(BrandSeeder::class);
 //        $this->call(ProductSeeder::class);
-//        $this->call(UserSeeder::class);
+        $this->call(UserSeeder::class);
     }
 }
